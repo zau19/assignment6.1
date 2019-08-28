e67079cc3f7d2b1344b0def682e3613f79b3484f
zau19 <54337274+zau19@users.noreply.github.com>
2019-08-28 19:11:05 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var firstName = prompt('what is your first name?');
+console.log(firstName);
+
+var lastName = prompt('whatis your last name?');
+console.log(lastName);
+
+var age = prompt('what is your age?');
+console.log(age);
+
+console.log("your firstname is " + firstName+"."+"your lastname is " + lastName+".");
+console.log(firstName +" "+lastName +" is "+ age +" year old.");
+
